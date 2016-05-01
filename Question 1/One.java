299b1ef14d1fc3dcfb0d748be2da3e99c672a7f4
Vignesh Rajmohan <vpr1999@yahoo.com>
2016-04-30 21:17:45 -0400

    Update One.java

--- Question 1/One.java
+++ Question 1/One.java
@@ -1,6 +1,11 @@
 
 /**
- * My Solution to Euler Project Question 1
+ * My Solution to Euler Project Question 1: 
+ * 
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, 
+ * we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ * 
  * 
  * @author Vignesh Rajmohan
  * @version April 28, 2016
