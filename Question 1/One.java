a40b68eeb147a5391ff59b5cd3e17d2e59ca5571
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-08-26 19:11:58 +0530

    Concise code that no one asked for

--- Question 1/One.java
+++ Question 1/One.java
@@ -15,18 +15,11 @@ public class One
     public static void main()
     {
         int sum = 0;
-        for (int i = 0; i < 1000; i++)
-        {
-            if (i % 3 == 0)
-            {
-                sum += i;
-            }
-            else if (i % 5 == 0)
-            {
-                sum += i;
-            }
-        }
-        
-        System.out.print(sum);
+		for(int i = 1; i < 1000; i++) {
+			if(i % 3 == 0 || i % 5 == 0) {
+				sum += i;
+			}
+		}
+		System.out.println(sum);
     }
 }
